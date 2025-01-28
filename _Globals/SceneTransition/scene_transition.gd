0e701635c247683639cd9445ee0fe07ec89af800
DawidKrolikiewicz <dawid.krolikiewicz@student.up.krakow.pl>
2025-01-28 17:31:34 +0100

    clicksound

--- _Globals/SceneTransition/scene_transition.gd
+++ _Globals/SceneTransition/scene_transition.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 
 func change_scene(new_scene: String) -> void:
 	print_rich("[color=yellow]%s[/color]" % new_scene)
@@ -12,3 +13,7 @@ func change_scene(new_scene: String) -> void:
 	animation_player.play("fade_out")
 	await animation_player.animation_finished
 	visible = false
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("RMB"):
+		audio_stream_player.play()
