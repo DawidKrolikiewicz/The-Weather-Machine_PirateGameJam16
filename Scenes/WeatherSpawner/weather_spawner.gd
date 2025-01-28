6277ebc5049937d173814cb689dec0421cd2a426
Burdelstary <nikodem.polak.2005@gmail.com>
2025-01-28 14:19:01 +0100

    Positive and neutral weather + better random
    Easier to add new weathers now, two arrays (positive and neutral), each has a chance to occur - 80/20 at first, 30/70 later. Each specific weather has the same chance to occur.

--- Scenes/WeatherSpawner/weather_spawner.gd
+++ Scenes/WeatherSpawner/weather_spawner.gd
@@ -1,31 +1,34 @@
 extends Node2D
 class_name WeatherSpawner
 
-@export var weathers_to_spawn: Array[WeatherData] = []
-@export var base_weather_weights: Array[float] = []
-@export var weight_modifiers: Array[float] = [] 
-@export var min_weight: float = 0.0 
+@export var positive_weathers: Array[WeatherData] = [] 
+@export var neutral_weathers: Array[WeatherData] = [] 
+
+@export var positive_chance: float = 80.0 
+@export var neutral_chance: float = 20.0  
+
+@export var positive_chance_modifier: float = -1.0  
+@export var neutral_chance_modifier: float = 1.0  
+
+@export var min_positive_chance: float = 30.0 
+@export var max_neutral_chance: float = 70.0  
+
+@export var min_weight: float = 0.0  
 @export var max_weight: float = 100.0 
-@export var max_rainy_weight: float = 70.0
-@export var min_sunny_weight: float = 30.0
 
 @onready var timer: Timer = $Timer
 
-var weathers_array: Array[WeatherZone] = []
+var weathers_array : Array[WeatherZone] = []
 var weather_zone: PackedScene = preload("res://Scenes/WeatherZone/weather_zone.tscn")
-var weather_weights: Array[float] = []
 
 func _ready() -> void:
-	if weathers_to_spawn.size() != base_weather_weights.size():
-		push_error("weathers_to_spawn and base_weather_weights must have the same size!")
-		return
-	for weight in base_weather_weights:
-		weather_weights.append(float(weight))
 	randomize_timer_wait_time()
 	timer.start()
+
 	spawn_weather_zone()
 	spawn_weather_zone()
 	spawn_weather_zone()
+
 
 func _on_timer_timeout() -> void:
 	randomize_timer_wait_time()
@@ -46,18 +49,21 @@ func spawn_weather_zone() -> void:
 	instance.disolve_finished.connect(_on_weather_disolved)
 	
 func _on_weather_disolved(zone: WeatherZone) -> void:
-	weathers_array.erase(zone)
+	weathers_array.erase(zone) 
 	zone.queue_free()
 	
 func get_random_weather_data() -> WeatherData:
-	var total_weight = calculate_sum(weather_weights)
-	var random_value = randi() % total_weight
-	var cumulative_weight = 0.0
-	for i in range(weathers_to_spawn.size()):
-		cumulative_weight += weather_weights[i]
-		if random_value < cumulative_weight:
-			return weathers_to_spawn[i]
-	return weathers_to_spawn[0]
+	var total_chance = positive_chance + neutral_chance
+	var random_value = randf() * total_chance
+
+	if random_value < positive_chance:
+		return select_weather_from_group(positive_weathers)
+	else:
+		return select_weather_from_group(neutral_weathers)
+		
+func select_weather_from_group(weathers: Array[WeatherData]) -> WeatherData:
+	var index = randi() % weathers.size()
+	return weathers[index]
 
 func calculate_sum(array: Array[float]) -> int:
 	var total = 0
@@ -66,20 +72,16 @@ func calculate_sum(array: Array[float]) -> int:
 	return total
 	
 func update_weather_weights() -> void:
-	for i in range(weather_weights.size()):
-		
-		if weathers_to_spawn[i].name == "Rain" and weather_weights[i] < max_rainy_weight:
-			weather_weights[i] += weight_modifiers[i]
-			if weather_weights[i] > max_rainy_weight:
-				weather_weights[i] = max_rainy_weight
-				
-		elif weathers_to_spawn[i].name == "Sun Rays" and weather_weights[i] > min_sunny_weight:
-			weather_weights[i] += weight_modifiers[i] 
-			if weather_weights[i] < min_sunny_weight:
-				weather_weights[i] = min_sunny_weight
-				
-		if weather_weights[i] < min_weight:
-			weather_weights[i] = min_weight
-		elif weather_weights[i] > max_weight:
-			weather_weights[i] = max_weight
-	print("Updated weights:", weather_weights)
+	positive_chance = clamp(
+		positive_chance + positive_chance_modifier,
+		min_positive_chance,
+		100
+	)
+
+	neutral_chance = clamp(
+		neutral_chance + neutral_chance_modifier,
+		0,
+		max_neutral_chance
+	)
+
+	print("Positive chance:", positive_chance, "Neutral chance:", neutral_chance)
