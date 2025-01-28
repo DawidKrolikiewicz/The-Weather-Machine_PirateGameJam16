c1e12ecfabf30933c2fb564a30dcbff6c96d60d5
DawidKrolikiewicz <dawid.krolikiewicz@student.up.krakow.pl>
2025-01-28 16:54:17 +0100

    Game Speed setting scaling

--- Scenes/WeatherZone/weather_zone.gd
+++ Scenes/WeatherZone/weather_zone.gd
@@ -7,9 +7,7 @@ signal disolve_finished(me)
 @export var data: WeatherData
 
 # VISUAL
-#@onready var weather_sprite: Sprite2D = $WeatherSprite
 @onready var visual: Node2D = $Visual
-
 
 # ROTATION
 @onready var axle: Node2D = $Axle
@@ -21,9 +19,6 @@ signal disolve_finished(me)
 @onready var effect_area_2d: Area2D = $EffectArea2D
 @onready var effect_collision: CollisionShape2D = $EffectArea2D/CollisionShape2D
 
-# LIFETIME
-@onready var lifetime_timer: Timer = $LifetimeTimer
-
 var direction: Vector2
 var rotation_vel: float = 0
 
@@ -31,6 +26,9 @@ var size: float
 
 var mouse_in: bool = false
 var follow: bool = false
+
+var life_start: bool = false
+var lifetime: int
 
 func _ready() -> void:
 	SignalBus.tick.connect(_on_tick_timer_timeout)
@@ -52,15 +50,15 @@ func _ready() -> void:
 	size = randi_range(data.min_full_size, data.max_full_size)
 	var tween = get_tree().create_tween()
 	tween.set_parallel(true)
-	tween.tween_property(drag_collision.shape, "radius", size/5, data.form_time)
-	tween.tween_property(drag_collision.shape, "height", size, data.form_time)
-	tween.tween_property(effect_collision.shape, "radius", size/5, data.form_time)
-	tween.tween_property(effect_collision.shape, "height", size, data.form_time)
-	tween.tween_property(visual, "scale", Vector2(0.25, 0.25) * size/200, data.form_time)
+	tween.tween_property(drag_collision.shape, "radius", size/5, data.form_time / (Settings.game_speed/100))
+	tween.tween_property(drag_collision.shape, "height", size, data.form_time / (Settings.game_speed/100))
+	tween.tween_property(effect_collision.shape, "radius", size/5, data.form_time / (Settings.game_speed/100))
+	tween.tween_property(effect_collision.shape, "height", size, data.form_time / (Settings.game_speed/100))
+	tween.tween_property(visual, "scale", Vector2(0.25, 0.25) * size/200, data.form_time / (Settings.game_speed/100))
 	await tween.finished
 	
-	lifetime_timer.wait_time = randf_range(data.min_lifetime, data.max_lifetime)
-	lifetime_timer.start()
+	lifetime = randf_range(data.min_lifetime, data.max_lifetime)
+	life_start = true
 	
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("RMB") and mouse_in:
@@ -76,10 +74,10 @@ func _process(delta: float) -> void:
 	
 	if !follow:
 		direction = (direction_marker.global_position - global_position).normalized()
-		position += direction * data.speed * delta
+		position += direction * data.speed * delta * (Settings.game_speed / 100)
 	else:
 		direction = (get_global_mouse_position() - global_position).normalized()
-		position += direction * min(data.speed * delta * data.speed_mult, (get_global_mouse_position() - global_position).length())
+		position += direction * min(data.speed * delta * data.speed_mult * (Settings.game_speed / 100), (get_global_mouse_position() - global_position).length())
 	
 	if mouse_in:
 		SignalBus.display_weather_info.emit(data)
@@ -94,15 +92,19 @@ func _on_tick_timer_timeout() -> void:
 	var overlapping_poi = effect_area_2d.get_overlapping_areas()
 	for o in overlapping_poi:
 		o.owner.on_tick(data)
+	
+	if life_start:
+		lifetime -= (Settings.game_speed / 100)
+		if lifetime <= 0:
+			var tween = get_tree().create_tween()
+			tween.set_parallel(true)
+			tween.tween_property(drag_collision.shape, "radius", 0, data.dissolve_time / (Settings.game_speed/100))
+			tween.tween_property(drag_collision.shape, "height", 0, data.dissolve_time / (Settings.game_speed/100))
+			tween.tween_property(effect_collision.shape, "radius", 0, data.dissolve_time / (Settings.game_speed/100))
+			tween.tween_property(effect_collision.shape, "height", 0, data.dissolve_time / (Settings.game_speed/100))
+			tween.tween_property(visual, "scale", Vector2.ZERO, data.dissolve_time / (Settings.game_speed/100))
+			await tween.finished
+			disolve_finished.emit(self)
 
-func _on_lifetime_timer_timeout() -> void:
-	var tween = get_tree().create_tween()
-	tween.set_parallel(true)
-	tween.tween_property(drag_collision.shape, "radius", 0, data.dissolve_time)
-	tween.tween_property(drag_collision.shape, "height", 0, data.dissolve_time)
-	tween.tween_property(effect_collision.shape, "radius", 0, data.dissolve_time)
-	tween.tween_property(effect_collision.shape, "height", 0, data.dissolve_time)
-	tween.tween_property(visual, "scale", Vector2.ZERO, data.dissolve_time)
-	await tween.finished
-	disolve_finished.emit(self)
+
 	
